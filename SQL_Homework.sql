cc35f36637689688fd414369cef50910b7759719
lprymak <46330017+lprymak@users.noreply.github.com>
2019-04-10 15:58:37 -0500

    Clarify text

--- SQL_Homework.sql
+++ SQL_Homework.sql
@@ -87,8 +87,25 @@ WHERE first_name='HARPO';
 
 -- 5a ----------------------------------------------------------------------------
 
-/*Displays CREATE TABLE statment from previously created address table*/
+/*Displays CREATE TABLE statment for previously created 'address' table*/
 SHOW CREATE TABLE address;
+
+/*Copy field from above to recreate table*/
+CREATE TABLE `address` (
+  `address_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) NOT NULL,
+  `address2` varchar(50) DEFAULT NULL,
+  `district` varchar(20) NOT NULL,
+  `city_id` smallint(5) unsigned NOT NULL,
+  `postal_code` varchar(10) DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  `location` geometry NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`address_id`),
+  KEY `idx_fk_city_id` (`city_id`),
+  SPATIAL KEY `idx_location` (`location`),
+  CONSTRAINT `fk_address_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=606 DEFAULT CHARSET=utf8;
 
 -- 6a ----------------------------------------------------------------------------
 
